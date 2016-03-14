c3953473a680490cfba54c20fc1fe09e1841647d
Rashiv Romio Bhusal <ROMIO@rashivs-macbook-pro.local>
2016-03-14 14:33:06 +1100

    Initial Commit

---
+++ iBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBank
+//
+//  Created by Rashiv Romio Bhusal on 14/03/2016.
+//  Copyright © 2016 Rashiv Romio Bhusal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
